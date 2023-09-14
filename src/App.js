66e3cec822feb97a85d3bc1bb7d2cac34f885271
yejin-km <124483450+yejin-km@users.noreply.github.com>
2023-09-13 23:56:06 -0400

    added github link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ export default function App() {
       <div>
         <h1>Weather App</h1>
         <Search />
+        <p>
+          <a href="https://github.com/yejin-km/react-weather">
+            Open source code
+          </a>{" "}
+          by Yejin Kim
+        </p>
       </div>
     </div>
   );
